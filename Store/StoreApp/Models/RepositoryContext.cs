89baac5acc3007be556852c3cbca7d1676b53765
Oguzhan Yıldız <oguzy2020@gmail.com>
2023-12-12 19:08:29 +0300

    connected to sqlite db and added migrations

---
+++ Store/StoreApp/Models/RepositoryContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreApp.Models
+{
+    public class RepositoryContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        // Db için gerekli 
+        public RepositoryContext(DbContextOptions<RepositoryContext> options)
+        : base(options)
+        {
+
+        }
+
+        // bu method db de veri varsa çalışmaz ama yoksa içindeki kaydı db ye ekler
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+            .HasData(
+                new Product() { ProductId = 1, ProductName = "Computer", Price = 17_000 },
+                new Product() { ProductId = 2, ProductName = "Keyboard", Price = 1_000 },
+                new Product() { ProductId = 3, ProductName = "Mouse", Price = 500 },
+                new Product() { ProductId = 4, ProductName = "Monitor", Price = 7_000 },
+                new Product() { ProductId = 5, ProductName = "Deck", Price = 1_500 }
+
+            );
+        }
+
+    }
+}
--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews(); // MVC deki View kullanılmasını sağlar
+
+builder.Services.AddDbContext<RepositoryContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
+
+});
 
 var app = builder.Build();
 
